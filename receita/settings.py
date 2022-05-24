413a4d9a1dc3c9874f55ced255741ddcb3f102e8
Lucas Almeida <lucasalmeida4522@gmail.com>
2022-05-24 12:20:01 -0300

    Atualizando diretório do templates

--- receita/settings.py
+++ receita/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'receita.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'recitas/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'receitas/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
